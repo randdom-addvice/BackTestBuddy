62d3d6a6d875de159dec2481029239351d7b7fb5
randdom-addvice <akoladefaruq42@gmail.com>
2024-01-14 01:08:38 +0100

    style: add plans page styling

--- app/src/pages/PlansPage.tsx
+++ app/src/pages/PlansPage.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import DashBoardLayout from "@/components/dashboard/layout/DashBoardLayout";
 import { StyledContainer } from "@/styles/globalElements";
+import PlansContainer from "@/components/plans/PlansContainer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  overflow-y: auto;
+  width: 100%;
+`;
 
 const PlansPage = () => {
   return (
     <DashBoardLayout>
-      <StyledContainer margin="0">
-        <h1>plans</h1>
-      </StyledContainer>
+      <Container>
+        <PlansContainer />
+      </Container>
     </DashBoardLayout>
   );
 };
